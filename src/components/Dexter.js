7c4b705757212c2b05603f0bb815d5801cc46d83
sonicspeed123 <joshuasahagun1996@yahoo.com>
2016-08-18 17:38:49 -0600

    PokeInfo will show pokemon info

--- src/components/Dexter.js
+++ src/components/Dexter.js
@@ -1,6 +1,10 @@
 //Main background for pokedex
 import React from 'react';
 //will style later
+import Filter from './Filter';
+import List from './List';
+import PokeInfo from './PokeInfo';
+
 
 class Dexter extends React.Component{
 	constructor(props){
@@ -37,15 +41,14 @@ class Dexter extends React.Component{
 		], filteredPoke:[], filter:[]}
 	}
 
+
 	render(){
 		return(
-			<div>
-			//pokeinfo wll render individually selected pokemon
-				<PokeInfo />
-			//list will render a flitered/unfiltered list of pokemon	
-				<List />
-			//filter will set setting for filter function for list
-				<Filter />
+			<div className='row'>
+				<PokeInfo currentMon={this.state.pokemon[2]} />
+{//				<List />
+//				<Filter />
+}
 			</div>
 			)
 	}
--- src/components/PokeInfo.js
+++ src/components/PokeInfo.js
@@ -1,22 +1,20 @@
-import React from 'React';
+import React from 'react';
 
-class PokeInfo extentds React.component{
-	constructor(props){
-		super(props);
-	}
+class PokeInfo extends React.Component{
 //take currently selected pokemon
-	
-
 	render() {
-    let { currentMon } = this.props;
-    
-    return (<div></div>)
+		let {id, name, description, type1, type2, bodyType, abilities} = this.props.currentMon;
+    return (
+    	<div>
+	    	<img className='col' src={"../src/images/"+ id +".jpg"} alt={name} />
+	    	<p className='col'>{name}</p>
+	    	<p className='col'>{description}</p>
+	    	<p className='col'>{type1}/{type2}</p>
+	    	<p className='col'>{bodyType}</p>
+	    	<p className='col'>{abilities}</p>
+    	</div>
+    )
 	}
-//find pic
-//find description
-//find type and abilies
-
-//render pic in div
-//render des in div
-//render type and abil in div
 }
+
+export default PokeInfo;
