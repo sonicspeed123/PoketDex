087217a66092b1d085af7fb3fab0e43a4efad208
Colby Almond <colby@overstock.com>
2016-08-22 20:33:46 -0600

    Add files via upload

---
+++ Filter.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import List from './List'';
+
+/*const pokeFilter = (pokemon, cb) => {
+  if (this.refs.isCheckedInput.checked) {
+      this.props.onPokeMonInput(
+      this.refs.isCheckedInput.value,
+      this.refs.isCheckedInput.checked
+    );
+  } else {
+    this.props.filteredPoke('', false);
+  }
+},
+
+render() {
+
+  return (
+    <label>
+      <input
+        type="CheckBox",
+        value={this.props.value}
+        checked={this.props.isChecked}
+        ref="isCheckedInput"
+        onChange={this.handleChange}
+      />
+      {this.props.value}
+    </label>
+    );
+  }
+});*/
+
+//pull filteredPoke
+
+
+
+
+  )
+}
+
+
+);
+
+export default Filter;
